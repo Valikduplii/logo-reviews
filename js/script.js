c30018b57a0391d57bca613c8a1173cf1f95c80e
Valikduplii <69109832+Valikduplii@users.noreply.github.com>
2020-10-07 13:49:26 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -8,26 +8,79 @@ $(document).ready(function () {
 			$(this).toggleClass('active');
 			leftside();
 		});
-	});
-$('.header__burger').click(function (event) {
-   $('.header__burger, .header').toggleClass('active');
-   $('body').toggleClass('lock');
-   $('.content').toggleClass('active');
-});
+   });
+   
+   $(window).resize(function() {
+      if ($(window).width() < '780'){
+         $('.header__burger').click(function (event) {
+            $('.header__burger, .header').toggleClass('active');
+            $('body').toggleClass('lock');
+            $('.content').toggleClass('active');
+            $('.wrapper').toggleClass('active');
+            $('.mobile-window__catalog').css('display', 'none');
+            $('.mobile-window__info').css('display', 'block');
+            $('.filter__mobile').css('display', 'block');
+         }); 
+         $('.button-catalog').click( function (event) {
+            $('.header__burger, .header').toggleClass('active');
+            $('body').toggleClass('lock');
+            $('.content').toggleClass('active');
+            $('.mobile-window__catalog').css('display', 'block');
+            $('.mobile-window__info').css('display', 'none');
+            $('.filter__mobile').css('display', 'block');   
+            $('.wrapper').toggleClass('active');
+         });
+      } else if ($(window).width() > '772'){
+         $('.header__burger.active, .header.active').toggleClass('active');
+         $('body.lock').toggleClass('lock');
+         $('.content.active').toggleClass('active');
+         $('.wrapper.active').toggleClass('active');
+      }
+      });
+      $('.select').click(function(event) {
+         if(!$(this).hasClass('disabled')){
+            $('.select').not(this).removeClass('active').find('.select-options').slideUp(50);
+            $(this).toggleClass('active');
+            $(this).find('.select-options').slideToggle(50);
+         }
+      });
+      $('.select-options__value').click(function() {
+         if($(this).parents('.select').hasClass('content-search__select')){
+            $(this).parents('.select').find('.select-title__value').html('<span>'+$(this).html()+'</span>');
+         }else{
+            $(this).parents('.select').find('.select-title__value').html($(this).html());
+         }
+         if($.trim($(this).data('value'))!=''){
+            $(this).parents('.select').find('input').val($(this).data('value'));
+         }else{
+            $(this).parents('.select').find('input').val($(this).html());
+         }
+      });
+      $(document).click(function(e) {
+         if (!$(e.target).is(".select *")) {
+            $('.select').removeClass('active');
+            $('.select-options').slideUp(50);
+         };
+      });
+      $('.search__left').click(function(event) {
+         if(!$('.search__left').hasClass('active')){
+            $('.search__text').toggleClass('active');
+            $('.search__arrow').toggleClass('active');
+            $('.search__list').addClass('active');
+            $('.search__list').slideToggle(500);
+         }
+      });
+      $('.header__burger2').click(function (event) {
+         $('.menu').toggleClass('close');
+         $('.menu').slideToggle(500);
+         $('.header__burger2').toggleClass('active');
+         $('.main').toggleClass('active');
+         // $('.main').css('min-height', '2200px');
+      });
+
 $('.mobile-arrow').click(function (event) {
    $(this).parent().find('.mobile-sub-menu__list').toggleClass('active');
    $(this).parent().find('.mobile-arrow').toggleClass('active');
-});
-$('.search__left').click(function (event) {
-   $('.search__text').toggleClass('active');
-   $('.search__arrow').toggleClass('active');
-   $('.search__list').toggleClass('active');
-   $('search__list').slideDown();
-});
-$('.header__burger2').click(function (event) {
-   $('.menu').toggleClass('close');
-   $('.header__burger2').toggleClass('active');
-   $('.main').toggleClass('active');
 });
 $('.slider-content-img').click(function (event) {
    $(this).parents('.slider-conten-item').find('.slider-content__text').addClass('active');
